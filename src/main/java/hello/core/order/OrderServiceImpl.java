33b18f4bcd14a88186d8039d7821986b02abc34e
h3yon <khykhy1006@gmail.com>
2021-10-05 19:13:52 +0900

    feat: 생성자 주입 방식

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,16 +3,18 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    // 추상화에만 의지하도록 설계 -> 정률할인으로 변경 시 전혀 변경 필요 없음. 기능 확장 가능
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("memberRepository = " + memberRepository);
+        System.out.println("memberRepository = " + memberRepository);
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
@@ -20,13 +22,11 @@ public class OrderServiceImpl implements OrderService{
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
-        // 할인에 대해선 정해지지 않았으니까 설계가 잘 된 것
         int discountPrice = discountPolicy.discount(member, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 
-    // Test
     public MemberRepository getMemberRepository() {
         return memberRepository;
     }
