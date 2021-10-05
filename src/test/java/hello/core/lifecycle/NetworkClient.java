c0e7b4871ff72779cae6bbd376a327dab231325e
h3yon <khykhy1006@gmail.com>
2021-10-06 04:45:29 +0900

    feat: 빈 생명주기 + 최신 방법

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient{
 
     private String url;
@@ -28,6 +31,7 @@ public class NetworkClient{
         System.out.println("close = " + url);
     }
 
+    @PostConstruct
     public void init() {
         // 의존 관계 끝나면 주입. 싱글톤
         System.out.println("NetworkClient.afterPropertiesSet");
@@ -35,6 +39,7 @@ public class NetworkClient{
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.destroy");
         disconnect();
