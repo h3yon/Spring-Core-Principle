f0e134610882f709c823e0ed52f04d28c9806469
h3yon <khykhy1006@gmail.com>
2022-04-20 23:30:34 +0900

    [8-4]프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제점

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,41 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind(){
+
+        //PrototypeBean 을 Component 로 등록 안 해줘도 여기 Context 파라미터에 넣으면 스캔 대상이 된다.
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+
+        Assertions.assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean{
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
