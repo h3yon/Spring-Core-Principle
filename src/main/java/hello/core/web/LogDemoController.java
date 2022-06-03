b94092fed1e5e01b460613486854e3f4b844d6c3
h3yon <khykhy1006@gmail.com>
2022-06-03 20:04:16 +0900

    [8-7]스코프와 프로바이더 - 에러 해결

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -1,8 +1,8 @@
 package hello.core.web;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,15 +13,16 @@ import lombok.RequiredArgsConstructor;
 @Controller
 @RequiredArgsConstructor
 public class LogDemoController {
-    public final LogDemoService logDemoService;
-    //request 요청이 아직 안 됐는데 request 스코프인 myLogger에게 내놓으라고 하면 에러남.
-    private final MyLogger myLogger;
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
-        String requestURL = request.getRequestURL().toString();
-        myLogger.setRequestURL(requestURL);
+        MyLogger myLogger = myLoggerProvider.getObject();
+        String requestUrl = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestUrl);
+
         myLogger.log("controller test");
         logDemoService.logic("testId");
         return "OK";
